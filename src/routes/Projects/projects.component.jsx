c58b90e9774df99e74ddb2e2d8d424b4ead75338
Edward <herreraed93@gmail.com>
2023-03-29 11:01:45 -0700

    fix project mapping to page

--- src/routes/Projects/projects.component.jsx
+++ src/routes/Projects/projects.component.jsx
@@ -1,10 +1,21 @@
 import { Outlet } from "react-router-dom";
+import projects from '../../routes/Projects/projects.js'
+import './projects.style.css'
 
 const Projects=()=>{
     return(
         <div>
         <Outlet/>
-        <h1>Here Are My PROJECTS</h1>
+        <h1 className="header">PROJECTS</h1>
+        {projects.map((project)=>{
+                 const { id, title } = project;
+                 return(
+                <div key={id}>
+                    <h1>{title}</h1>
+                    {/* <img src={imageUrl} alt={title}/> */}
+                    </div>
+                 )
+                 })}
         </div>
     )
 }
